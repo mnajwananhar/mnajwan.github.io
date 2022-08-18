ececd26b0fe77abacb307b365d239b2a20272407
dejanma <mnajwananhardej@gmail.com>
2022-08-18 11:58:18 +0700

    upload pertama

---
+++ README.md
@@ -0,0 +1,2 @@
+# Repository-Baru
+# dejanmna2.github.io
---
+++ js/main.js
@@ -0,0 +1,14 @@
+let openMenu = document.querySelector(".hamburger");
+let menuWrapper = document.querySelector(".menu-wrapper")
+openMenu.addEventListener("click", function(){
+    {
+        openMenu.classList.toggle("fa-xmark")
+        menuWrapper.classList.toggle("active")
+        
+    }
+    
+
+   
+})
+
+
